89a70759e9eb37732b5d4014bce8d10d401b6f07
dmycse <dmycse@gmail.com>
2025-04-27 16:20:05 +0300

    tailwind: refactor tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-import { colors } from './node_modules/@sanity/runtime-cli/node_modules/chalk/source/index.d';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
@@ -11,28 +10,7 @@ const config: Config = {
       center: true,
       padding: '1rem',
     },
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1440px',
-    },
-    extend: {
-      fontFamily: {
-        rajdhani: ['var(--font-rajdhani)', 'sans-serif'],
-        mona: ['var(--font-mona)', 'sans-serif'],
-      },
-      colors: {
-        primary: {
-          DEFAULT: '#222222',
-          hover: '#2f2f2f'
-        },
-        accent: {
-          DEFAULT: 'ed1d24',
-          hover: '#d6001c'
-        }
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
