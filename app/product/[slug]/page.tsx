8aab7f5b4f376d966c377ac81e882b9869f1edd6
dmycse <dmycse@gmail.com>
2025-04-28 21:36:14 +0300

    app: refactor and feat ProductPage

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,12 +1,31 @@
-type ProductPageProps = {
-  params: {
-    slug: string
-  }
-}
+import { notFound } from "next/navigation";
+import { sanityFetch } from "@/sanity/lib/live";
+import { PRODUCT_QUERY } from '@/sanity/lib/queries';
+import { BikeDeatils } from '@/components/BikeDeatils';
 
-function ProductPage({ params: { slug } }: ProductPageProps) {
+type ProductPageProps = {
+  params: Promise<{ slug: string }>;
+};
+
+// const getProduct = async (slug: string) => {
+//   return await client.fetch(PRODUCT_QUERY, { slug });
+// };
+
+async function ProductPage ({ params }: ProductPageProps) {
+
+  // const product = await getProduct(slug);
+
+  const { data: bike } = await sanityFetch({
+    query: PRODUCT_QUERY,
+    params: await params,
+  });
+
+  if (!bike) {
+    notFound();
+  }
+
   return (
-    <div>slug</div>
+    <BikeDeatils bike={bike} />
   )
 }
 
