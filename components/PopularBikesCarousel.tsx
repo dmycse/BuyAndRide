4f4c81745d0e5fea45bf6d0ed66b4d9b06d3e072
dmycse <dmycse@gmail.com>
2025-04-28 11:52:40 +0300

    components: add navigation to bikes images carousel

--- components/PopularBikesCarousel.tsx
+++ components/PopularBikesCarousel.tsx
@@ -4,9 +4,10 @@ import { BikeCard } from "@/components/BikeCard";
 import type { Product } from "@/components/PopularBikes";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 type PopularBikesCarouselProps = {
   bikes: Product[]
@@ -28,6 +29,7 @@ export const PopularBikesCarousel = ( { bikes }: PopularBikesCarouselProps) => {
 
   return (
     <Swiper 
+      modules={[Navigation, Pagination]}
       slidesPerView={1} 
       spaceBetween={30}
       breakpoints={{
@@ -36,8 +38,8 @@ export const PopularBikesCarousel = ( { bikes }: PopularBikesCarouselProps) => {
         960: { slidesPerView: 3 },
         1440: { slidesPerView: 4 },
       }} 
-      pagination={{ clickable: true }} 
-      modules={[Pagination]}
+      pagination={{ clickable: true }}
+      navigation
       className="popular-bikes-slider mb-8"
   >
       {bikePropsforSwiper.map(bike => (
