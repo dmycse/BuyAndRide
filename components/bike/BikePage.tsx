0e6853af2aef852337d223b817f2d6b049454b8b
dmycse <dmycse@gmail.com>
2025-04-30 20:20:06 +0300

    components: create BikePage component

---
+++ components/bike/BikePage.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import { BikeDetais } from './BikeDetais';
+import { Product } from '@/sanity/types';
+import { ChevronLeft } from 'lucide-react';
+import type { TBike } from '@/components/PopularBikes';
+
+export type BikeDeatilsProps = {
+  bike: TBike;
+};
+
+export const BikePage = ({ bike }: BikeDeatilsProps) => {
+  return (
+    <section className='pt-2 pb-32'>
+        <div className="custom-container">
+          <div className="flex flex-col gap-4">
+            <div className="">
+              <Link href="/" className='flex items-center gap-1 font-semibold hover:text-accent'>
+                <ChevronLeft size={25} /> 
+                <span>Back to home</span>
+              </Link>
+            </div>
+            <BikeDetais bike={bike} />
+          </div>
+        </div>
+    </section>
+  )
+}
