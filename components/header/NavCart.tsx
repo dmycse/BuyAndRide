a4d43028194525b67e1cbd85c223117581c5fecf
dmycse <dmycse@gmail.com>
2025-04-30 20:14:25 +0300

    components: create NavCart component

---
+++ components/header/NavCart.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { CgShoppingCart } from 'react-icons/cg';
+import { useShoppingCart } from 'use-shopping-cart';
+
+export const NavCart = () => {
+
+  const { cartCount, handleCartClick } = useShoppingCart();
+
+  return (
+    <button
+     
+      className="relative group cursor-pointer"
+      onClick={() => handleCartClick()}
+    >
+      <CgShoppingCart size={30} className='text-2xl group-hover:text-accent' />
+      <span 
+        className='absolute top-0 right-0 w-4 h-4 flex-center
+                   text-xs text-white font-semibold bg-accent rounded-full'
+      >
+        {cartCount}
+      </span>
+    </button>
+  )
+}
