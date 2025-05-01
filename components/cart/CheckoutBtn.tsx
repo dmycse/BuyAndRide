6102d89c4d8ff5e7d1e704b5d74abfabb5b79831
dmycse <dmycse@gmail.com>
2025-05-01 16:50:32 +0300

    components: feat CheckoutBtn by handleCheckout handler

--- components/cart/CheckoutBtn.tsx
+++ components/cart/CheckoutBtn.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import { useShoppingCart } from 'use-shopping-cart';
 
 export const CheckoutBtn = () => {
+
+  const { redirectToCheckout } = useShoppingCart();
+
+
+  const handleCheckout = async () => {
+    try {
+      const res = await redirectToCheckout();
+      
+      if (res?.error) {
+        console.log(res.error);
+        throw new Error(res.error);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <button
       type="button" 
       className="w-full btn btn-accent"
-      onClick={() => console.log('checkout')}
+      onClick={handleCheckout}
     >
       Proceed to checkout
     </button>
