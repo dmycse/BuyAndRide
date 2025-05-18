7c01a9b4e73a4cf3b7673c19c7ae729a15097e6d
dmycse <dmycse@gmail.com>
2025-05-18 12:41:37 +0300

    components: refactor CartItem

--- components/cart/CartItem.tsx
+++ components/cart/CartItem.tsx
@@ -1,19 +1,21 @@
 'use client';
 
+import Link from "next/link";
 import Image from "next/image";
 import { urlFor } from "@/sanity/client";
 import type { CartEntry } from "use-shopping-cart/core";
 import { useShoppingCart } from "use-shopping-cart";
+import { slugify } from "@/lib/slugify";
 
-import { FaPlus, FaMinus, FaTrashCan } from 'react-icons/fa6'
+import { FaPlus, FaMinus, FaTrashCan } from 'react-icons/fa6';
 
 export const CartItem = ({ item }: { item: CartEntry }) => {
   
-  const {removeItem, incrementItem, decrementItem} = useShoppingCart();
+  const { removeItem, incrementItem, decrementItem, handleCartClick } = useShoppingCart();
     
   return (
     <article
-      className="mb-4 w-full h-30 flex-center justify-between shadow-sm"
+      className="mb-4 w-full h-30 flex items-center gap-8 max-md:gap-2 shadow-sm"
     >
       <div className="w-[110px] h-[110px] relative">
         <Image 
@@ -26,9 +28,15 @@ export const CartItem = ({ item }: { item: CartEntry }) => {
         /> 
       </div>
 
-      <div className="w-full ml-2 max-w-45 flex flex-col justify-center gap-4">
+      <div className="flex-1 w-full ml-2 flex flex-col justify-center gap-4">
         <div className="flex justify-between">
-          <h5>{item.name}</h5>
+          <Link 
+            href={`/product/${slugify(item.name)}`} 
+            className="text-primary hover:text-accent"
+            onClick={() => handleCartClick()}
+          >
+            {item.name}
+          </Link>
           <button 
             className="cursor-pointer text-gray-400 hover:text-accent"
             onClick={() => removeItem(item.id) }
