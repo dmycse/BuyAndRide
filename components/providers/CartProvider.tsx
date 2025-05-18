9d9840625190177cf62cb472ea72f7dccc2c937b
dmycse <dmycse@gmail.com>
2025-05-18 23:25:39 +0300

    components: update payments results url

--- components/providers/CartProvider.tsx
+++ components/providers/CartProvider.tsx
@@ -2,14 +2,17 @@
 
 import { CartProvider as CProvider } from "use-shopping-cart"; 
 
+const paymentSuccessUrl = "https://buyandride.vercel.app/stripe/payment-success";
+const paymentErrorUrl = "https://buyandride.vercel.app/stripe/payment-error"; 
+
 export const CartProvider = ({ children }: { children: React.ReactNode}) => {
   return (
     <CProvider
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY!}
-      successUrl="http://localhost:3000/stripe/payment-success"
-      cancelUrl="http://localhost:3000/stripe/payment-error"
+      successUrl={paymentSuccessUrl}
+      cancelUrl={paymentErrorUrl}
       language="en-US"
       currency="USD"
       billingAddressCollection={true}
